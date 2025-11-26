4e0e3ec9b578d9e7bb11a21f45973e8a9f6ba078
Frederico Alves <31135848+frederico-alves@users.noreply.github.com>
2025-11-26 19:48:42 +0100

    feat: Models/Equipment added

---
+++ backend/src/EquipmentTracker.Api/Models/Equipment.cs
@@ -0,0 +1,25 @@
+using EquipmentTracker.Api.Models.Enums;
+
+namespace EquipmentTracker.Api.Models;
+
+public class Equipment
+{
+    // { get; set; } Read and write allowed
+    // { get; } Read only
+    // { get; private set; } Publicly readable, privately writable
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public ProductionState CurrentState { get; set; } = ProductionState.Red;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdateAt { get; set; } = DateTime.UtcNow;
+
+    // Navigation property - one-to-many relationship between
+    // EquipmentTracker and StateChange
+    // ICollection - interface representing a collection of <StateChange>
+    // = new List<StateChange>() - initialized to empty list
+    // EF Core will create a foreign key in the StateChange table 
+    // pointing back to EquipmentTracker, allowing queries like:
+    // var history = equipment.StateChanges.ToList();
+    public ICollection<StateChange> StateChanges { get; set; } = new List<StateChange>();
+}
